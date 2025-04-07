64c6229bb35bf1cc25520e30352f89d02f403b5d
Lotfi Hocini <lotfi.hocini@hotmail.com>
2025-04-07 14:30:10 +0200

    first commit

---
+++ src/test/java/com/mie/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.mie;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void multiFailure(){
+        int a = 5;
+        int b = 2;
+        // assertTrue((a + b) == 3, "1 + 2 should be 3");
+        assertTrue(true);
+    }
+}
