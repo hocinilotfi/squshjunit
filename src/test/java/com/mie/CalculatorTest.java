d9c794638792c31e8ca56dde505982252123163c
Lotfi Hocini <lotfi.hocini@hotmail.com>
2025-04-08 11:20:14 +0200

    add jenkins file

--- src/test/java/com/mie/CalculatorTest.java
+++ src/test/java/com/mie/CalculatorTest.java
@@ -2,10 +2,12 @@ package com.mie;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
     @Test
+    @DisplayName("TC01 - test de somme")
     public void multiFailure(){
         int a = 5;
         int b = 2;
